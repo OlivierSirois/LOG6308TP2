4eac2e06ccd99193b8598fbb9f3dfe2d74b813ec
Alexandre BENTO <alexandre.bento@insa-lyon.fr>
2017-10-04 13:26:38 -0400

    PageRank fonctionnel avec plusieurs niveaux de références (manque la fonction pour retirer les auto-références

--- TP2.R
+++ TP2.R
@@ -1,4 +1,45 @@
-m = read.table("citeseer.rtable")
-d <- 0.85
-pr <- rep(1, dim(m)[1])
-(pr <- (1-d)/3 + (d * (m %*% (pr/colSums(m)))))
+# Load libraries
+library("expm")
+
+sum.powers.matrix <- function(m, n) {
+    powers <- c(1:n)
+    res <- Reduce('+', lapply(powers, function(x) m %^% x))
+    res[res > 1] <- 1
+
+    return(res)
+}
+
+remove.autoreferences <- function(m) {
+    res <- m
+}
+
+pagerank.iteration <- function(refs, n, d, pr) {
+    # Number of articles
+    n.articles <- dim(refs)[1]
+
+    # n-level references
+    m <- sum.powers.matrix(refs, n)
+
+    # Compute PageRank
+    pr.res <- (1-d)/n.articles + (d * (m %*% (pr/colSums(m))))
+
+    return(pr.res)
+}
+
+# Read source file
+data <- read.table("citeseer.rtable")
+
+# Cast data to matrix
+references <- as.matrix(data)
+
+m <- matrix(c(0,1,1,0,0,1,1,0,0),3)
+pr <- rep(1,3)
+
+pr <- pagerank.iteration(m, 2, 0.85, pr)
+print(pr)
+
+pr <- pagerank.iteration(m, 2, 0.85, pr)
+print(pr)
+
+pr <- pagerank.iteration(m, 2, 0.85, pr)
+print(pr)
